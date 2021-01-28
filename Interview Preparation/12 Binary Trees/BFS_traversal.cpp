bfae20fa03d2734438dbb355fc94164da5673a5f
M Vineeth <44038287+vineethm1627@users.noreply.github.com>
2021-01-28 21:17:17 +0530

    added newline in BFS traversal

--- Interview Preparation/12 Binary Trees/BFS_traversal.cpp
+++ Interview Preparation/12 Binary Trees/BFS_traversal.cpp
@@ -6,6 +6,16 @@
     pop the first element and insert its children in the queue.
 
     keep repeating the above step.
+
+    added newline
+
+    input: 8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+    output:
+        BFS: 
+        8    
+        10 3
+        1 6 14
+        9 7 13
 */
 #include <iostream>
 #include <queue>
@@ -43,19 +53,31 @@ void BFS(node *root) {
     queue<node*> q;
     // push the root in the queue
     q.push(root);
+    // push NULL to indicate the need of newline
+    // NULL implies all the children of the previous level are popped out.
+    // It also implies all the nodes of the next level are currently in the queue.
+    q.push(NULL);
 
     while(!q.empty()) {
         node *front = q.front();
-        q.pop();
-        // display the data
-        cout << front->data << " ";
-        // push the children into the queue if they exist
-        // push the left child
-        if(front->left)
-            q.push(front->left);
-        // push the right child
-        if(front->right)
-            q.push(front->right);
+        if(front == NULL) {
+            cout << endl;
+            q.pop();
+            if(!q.empty())
+                q.push(NULL);
+        }
+        else {
+            q.pop();
+            // display the data
+            cout << front->data << " ";
+            // push the children into the queue if they exist
+            // push the left child
+            if(front->left)
+                q.push(front->left);
+            // push the right child
+            if(front->right)
+                q.push(front->right);
+        }
     }
 }
 
