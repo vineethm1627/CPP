dc8aede0c2dc8be27ca4a9ca67796d31b57622a0
M Vineeth <44038287+vineethm1627@users.noreply.github.com>
2021-01-29 21:53:43 +0530

    mirror tree using recursive swap

---
+++ Interview Preparation/12 Binary Trees/mirror_tree.cpp
@@ -0,0 +1,125 @@
+/*
+
+    Find the mirror image of the tree
+
+    Input
+    8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+
+    Output
+    8,
+    10,3,
+    1,6,14,
+    9,7,13,
+    8,
+    3,10,
+    14,6,1,
+    13,7,9,
+*/
+/*
+    O(n) solution for finding the diameter of the tree.
+    bottom up traversal, postorder traversal
+    we will store the height.
+*/
+/*
+    Breadth First Search (BFS) makes use of queue data structure.
+    Takes O(n) time.
+
+    insert the root in the queue.
+    pop the first element and insert its children in the queue.
+
+    keep repeating the above step.
+
+    added newline
+
+    input: 8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+    output:
+        BFS: 
+        8    
+        10 3
+        1 6 14
+        9 7 13
+*/
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class node {
+    public:
+        int data;
+        node *left;
+        node *right;
+
+        node(int data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+// preorder build of a tree
+node* buildTree() {
+    int data;
+    cin >> data;
+
+    if(data == -1)
+        return NULL;
+    
+    node *root = new node(data);
+    root->left = buildTree();
+    root->right = buildTree();
+
+    return root;
+}
+
+void mirror(node* root) {
+    if(root == NULL)
+        return;
+
+    swap(root->left, root->right);
+    mirror(root->left);
+    mirror(root->right);
+}
+
+void BFS(node *root) {
+    queue<node*> q;
+    // push the root in the queue
+    q.push(root);
+    // push NULL to indicate the need of newline
+    // NULL implies all the children of the previous level are popped out.
+    // It also implies all the nodes of the next level are currently in the queue.
+    q.push(NULL);
+
+    while(!q.empty()) {
+        node *front = q.front();
+        if(front == NULL) {
+            cout << endl;
+            q.pop();
+            if(!q.empty())
+                q.push(NULL);
+        }
+        else {
+            q.pop();
+            // display the data
+            cout << front->data << " ";
+            // push the children into the queue if they exist
+            // push the left child
+            if(front->left)
+                q.push(front->left);
+            // push the right child
+            if(front->right)
+                q.push(front->right);
+        }
+    }
+}
+
+int main() {
+   
+    node *root = buildTree();
+    cout << "original tree: " << endl;
+    BFS(root);
+    mirror(root);
+    cout << "mirror tree: " << endl;
+    BFS(root);
+
+    return 0;
+}
