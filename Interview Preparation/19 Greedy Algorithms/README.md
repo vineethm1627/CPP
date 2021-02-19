f77529a81fcc3ba4c15f3d78f321d47929d5273b
M Vineeth <44038287+vineethm1627@users.noreply.github.com>
2021-02-19 17:04:50 +0530

    max no of activities selection

--- Interview Preparation/19 Greedy Algorithms/README.md
+++ Interview Preparation/19 Greedy Algorithms/README.md
@@ -4,3 +4,4 @@ SNo | Name | Logic Used | Link |
 ----|------|------------|------|
 1 | Coin change problem | STL upper bound | [view](coin_change.cpp)
 2 | Coin change problem | DP, table[amount+1], check all coins | [view](coin_change_DP.cpp)
+3 | Activity Selection | sort acc. to finishing time | [view](activity_selection.cpp)
---
+++ Interview Preparation/19 Greedy Algorithms/activity_selection.cpp
@@ -0,0 +1,54 @@
+/*
+    You are given n activities (from 0 to n-1) with their start and finish times. Select the maximum number of activities that can be performed by a single person, assuming that a person can only work on a single activity at a time.
+    Input Format
+    The first line consists of an integer T, the number of test cases. For each test case, the first line consists of an integer N, the number of activities. Then the next N lines contain two integers m and n, the start and end time of each activity.
+    Constraints
+    1<=t<=50
+    1<=n<=10000
+    1<=A[i]<=100
+    Output Format
+    For each test case find the maximum number of activities that you can do.
+    Sample Input
+    1 
+    3
+    10 20
+    12 15
+    20 30
+    Sample Output
+    2
+    Explanation
+    Person can only do 0th and 2nd activities.
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, int> p1, pair<int, int> p2) {
+    return p1.second < p2.second; // sort in ascending order wrt finishing time.
+}
+
+int main() {
+    int t, n;
+    cin >> t;
+
+    while(t--) {
+        int count = 1;
+        cin >> n;
+
+        pair<int, int> arr[n];
+        for(int i = 0; i < n; i++) 
+            cin >> arr[i].first >> arr[i].second;
+        
+        sort(arr, arr + n, compare);
+
+        int st = arr[0].second; // finishing time of the shortest activity
+        for(int i = 1; i < n; i++) {
+            if(arr[i].first >= st) {
+                count++;
+                st = arr[i].second;
+            }
+        }
+        cout << count << '\n';
+    }
+    return 0;
+}
