b1fb35bdb854e312782a2ce27116ce5bc774e4f0
M Vineeth <44038287+vineethm1627@users.noreply.github.com>
2021-04-26 12:57:26 +0530

    CP template for comp prog

---
+++ Productivity Hacks/01 Basics/template_CP.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+typedef long long int lli;
+#define ppi pair<int, int>
+#define all(c) c.begin(), c.end() 
+using namespace std;
+
+void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void solve() {
+    // take user input
+}
+
+int main() {
+    fastIO();
+    int T;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+    return 0;
+}
